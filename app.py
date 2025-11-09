5f3ca6d0ec924bd29fc9dd02428a48278c5fb7b3
shagohalanthropus <240828483+shagohalanthropus@users.noreply.github.com>
2025-11-08 19:18:11 -0600

    Added some comments to app.py to improve readability (hopefully).

--- app.py
+++ app.py
@@ -13,8 +13,10 @@ from pprint import pprint
 
 
 app = Flask(__name__)
+# Load our .env file
 load_dotenv()
 
+# If any of the environment variables aren't in our .env file, exit with status code 1.
 try:
     apikey = os.environ["OPEN_WEBUI_APIKEY"]
     base_url = os.environ["OPEN_WEBUI_URL"]
@@ -24,25 +26,33 @@ except KeyError as e:
     exit(1)
 
 def translate_image(image_file):
+    # Set up PaddleOCR
     ocr = PaddleOCR(
         use_doc_orientation_classify=False,
         use_doc_unwarping=False,
         use_textline_orientation=False
     )
+    # Perform OCR and get our results
     result = ocr.predict(input=image_file.name)
 
+    # For each result, get the bounding boxes and text that were returned by PaddleOCR.
     for res in result:
         pprint(res)
         boxes = res["rec_boxes"].tolist()
         texts = res["rec_texts"]
         with Image.open(image_file) as img:
+            # b is for bounding boxes, t is for text
             for b, t in zip(boxes, texts):
+                # Translate our extracted text
                 translated_text = translate_text(t)["choices"][0]["message"]["content"]
                 draw = ImageDraw.Draw(img)
+                # Draw both the bounding boxes and translated text to our image.
+                # xy is the upper left corner of the bounding box.
                 draw.rectangle(b, fill="black", outline="green")
                 draw.text(xy=(b[0], b[1]), text=translated_text, fill="white")
             img.save(image_file.name)
 
+# Send our extracted text to Open-WebUI for translation
 def translate_text(text):
     endpoint = "/api/chat/completions"
     full_url = base_url.removesuffix("/") + endpoint
@@ -63,13 +73,14 @@ def translate_text(text):
     }
 
     response = requests.post(full_url, headers=headers, json=data)
-    pprint(response.json())
     return response.json()
 
+# The endpoint that Retroarch interacts with
 @app.route("/", methods=["POST"])
 def retroarch_to_open_webui():
     data = request.get_json(force=True)
 
+    # Retroarch sends images as a Base64 encoded string, so we have to decode it
     image_data = b64decode(data["image"])
     image_file = tempfile.NamedTemporaryFile(suffix=".png",delete=False)
 
@@ -78,6 +89,7 @@ def retroarch_to_open_webui():
 
     translate_image(image_file)
 
+    # Take our translated image and re-encode it with Base64 and send it back to Retroarch
     return jsonify({"image": b64encode(open(image_file.name, "rb").read()).decode()})
 
 if __name__ == "__main__":
